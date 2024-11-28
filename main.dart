49cf42b992d42937189944c43b6cd7631bed14ad
Jessica Ernst <jessica_ernst@outlook.de>
2024-11-28 11:42:23 +0100

    reformatted and better comment explanations and alternative solution

--- main.dart
+++ main.dart
@@ -5,15 +5,27 @@ void main() {
   // Anzahl der Sitzplätze pro Reihe
   const int seatsPerRow = 12;
 
-  // Berechnungen von Reihe, Sitzplatz und Preis
-  int row =
-      (ticketNumber - 1) ~/ seatsPerRow +
-      1; // Reihe berechnen mit ganzzahliger Division
-  int seat =
-      (ticketNumber - 1) % seatsPerRow +
-      1; // Sitz innerhalb der Reihe berechnen
-  int price =
-      (seat % 2 == 0) ? 15 : 12; // ermitteln des Preises für den Sitzplatz
+  // Um die aufgabe mit einem if-else-Statement zu lösen, müsste die price-Variable vorher deklariert werden und nullable gemacht werden
+  // bei der Aufgabenstellung kann es dann auch ein final sein da es nur einmal initialisiert wird
+  //final int? price;
+
+  // Reihe berechnen mit ganzzahliger Division
+  int row = (ticketNumber - 1) ~/ seatsPerRow + 1;
+
+  // Berechnet die Sitznummer innerhalb der Reihe basierend auf der Ticketnummer und der Anzahl der Sitze pro Reihe
+  int seat = (ticketNumber - 1) % seatsPerRow + 1;
+
+  // Preis berechnen mit ternärem Operator, stattdesssen könnte auch eine if-else-Verzweigung verwendet werden wenn man noch keinen ternären Operator kennt
+  // dazu müsste die price variabel vorher deklariert werden
+  int price = (seat % 2 == 0) ? 15 : 12;
+
+  /* 
+  das wäre die Lösung mit einem if-else-Statement
+  if (seat % 2 == 0) {
+    price = 15;
+  } else {
+    price = 12;
+  } */
 
   // Mehrzeiliger String mit Ticket-Informationen
   String ticketInfo = '''
